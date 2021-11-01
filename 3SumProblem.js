c1424e45f47ab742d87637f74a99719fc9882c5a
Jacob Melnick <jake.r.melnick@gmail.com>
2021-11-01 10:18:17 -0700

    3sumProblem

---
+++ 3SumProblem.js
@@ -0,0 +1,34 @@
+const threeSum = (num) => {
+let sum = [];
+num.sort((a, b) => a - b);
+
+for (let i = 0; i <= num.length - 3; i++) {
+    const curr = num[i];
+    if (curr === num[i - 1]) {
+        continue;
+    };
+
+    let left = i + 1;
+    let right = num.length - 1;
+
+    while (left < right) {
+        let total = curr + num[left] + num[right];
+        if (total < 0) {
+            left++;
+        } else if (total > 0) {
+            right--;
+        } else if (curr !== num[left] !== num[right]) {
+            sum.push([curr, num[left], num[right]]);
+            left++;
+            right--;
+        }
+    }
+}
+let unique = Array.from(new Set(sum.map(JSON.stringify)), JSON.parse)
+console.log(unique)
+return unique
+}
+
+
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
