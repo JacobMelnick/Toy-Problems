132f75eededf5ff5f974dbbfcc0aee92c068f401
Jacob Melnick <jake.r.melnick@gmail.com>
2022-01-06 12:07:59 -0800

    finished Luck

--- luck.js
+++ luck.js
@@ -1,35 +1,39 @@
 const luckBalance = (k, contests) => {
   let final = 0;
   let important = 0;
-  let smallest = [];
   let lowestNum = 0;
+  let smallest = [];
+
   for (let i = 0; i < contests.length; i++) {
     const curr = contests[i];
     final += curr[0];
     important += curr[1];
 
     if (curr[1] === 1) {
-      console.log(curr[0]);
       smallest.push(curr[0]);
-    }
-  }
+    };
+  };
+
+  smallest.sort((a, b) => a - b);
+  
   if (smallest.length >= k) {
     let length = smallest.length;
-    while (length >= k) {
+    while (length > k) {
       lowestNum += Math.min(...smallest);
+      lowestNum += Math.min(...smallest);
+      smallest.shift();
       length--;
     }
   }
-
   return final - lowestNum;
 };
 
 console.log(
-  luckBalance(8, [
+  luckBalance(5, [
     [13, 1],
-    [10, 1],
-    [9, 1],
-    [8, 1],
+    [10, 1], // win
+    [9, 1], // win
+    [8, 1], // win
     [13, 1],
     [12, 1],
     [18, 1],
